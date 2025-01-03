8b2f0de92d06a8588379eb7f3b19daca12d7e2f9
pgiri <pgiri@pcs.com.np>
2025-01-03 15:22:38 +0545

    added counter for request count

--- SSEClient/Program.cs
+++ SSEClient/Program.cs
@@ -2,15 +2,16 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Start Connection");
 var client = new HttpClient();
-bool cancel = false;
+int count = 1;
 using var stream = await client.GetStreamAsync("https://localhost:7183/calender");
 using (StreamReader reader = new(stream))
 {
     while (!reader.EndOfStream)
     {
         var line = await reader.ReadLineAsync();
+        Console.WriteLine("Received Data Listening Count:"+count.ToString());
         Console.WriteLine(line);
-       
+        count++;
     }
     
 }
