b080abe562ced1e6e0d85088991e13019d1704a9
pgiri <pgiri@pcs.com.np>
2025-01-21 21:01:00 +0545

    notification serveice added

---
+++ SSECounterApi/NotificationManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SSECounterApi
+{
+    public class NotificationManager : INotificationManager
+    {
+        private readonly List<Dictionary<Notification, string[] >> _notifications;
+        public NotificationManager()
+        {
+            _notifications = new List<Dictionary<Notification, string[]>>();
+        }
+        public async Task Add(Notification @Notification,string[] users, CancellationToken cancellationToken)
+        {
+            await Task.Run(() =>
+            {
+                var val = new Dictionary<Notification, string[]>();
+                val.Add(@Notification, users);
+                _notifications.Add(val);
+            });
+        }
+
+        public  List<Notification> GetAll(string userid, CancellationToken cancellationToken)
+        {
+            // return  _notifications.SelectMany(x=> x.Keys).Where(user=> user.;
+            return null;
+        }
+    }
+    public interface INotificationManager
+    {
+        public Task Add(Notification @Notification,string[] users, CancellationToken cancellationToken);
+        public List<Notification> GetAll(string userid,CancellationToken cancellationToken);
+
+    }
+    public record Notification(string msg);
+}
---
+++ SSECounterApi/NotificationService.cs
@@ -0,0 +1,30 @@
+﻿namespace SSECounterApi
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserService _userService;
+        private readonly INotificationManager _notificationManager;
+        public NotificationService(IHttpContextAccessor httpContextAccessor, IUserService userService, INotificationManager notificationManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userService = userService;
+            _notificationManager = notificationManager;
+        }
+
+        public async Task AddNotification(Event @event, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ConnectAsync(CancellationToken cancellationToken, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface INotificationService
+    {
+        Task ConnectAsync(CancellationToken cancellationToken, string name);
+        Task AddNotification(Event @event, CancellationToken cancellationToken);
+    }
+}
