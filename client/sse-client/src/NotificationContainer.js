25a3e0c315c25d4ce685669f0909fd0be3e122fb
pgiri <pgiri@pcs.com.np>
2025-01-21 20:04:58 +0545

    sse counter working in react with toaster notification

--- client/sse-client/src/NotificationContainer.js
+++ client/sse-client/src/NotificationContainer.js
@@ -1,26 +1,22 @@
 
 import { ToastContainer, toast } from 'react-toastify';
-import { useEffect, useState } from 'react';
+import { useEffect,  useState } from 'react';
 export default function NotificationContainer() { 
-    const[data , UpdateData] = useState('data')
-    useEffect(() => {
-        // First, we need to create an instance of EventSource and pass the data stream URL as a
-        const es = new EventSource("/sse");
-        // parameter in its constructor
-       // Whenever the connection is established between the server and the client we'll get notified
-        es.onopen = () => console.log(">>> Connection opened!");
-       // Made a mistake, or something bad happened on the server? We get notified here
-        es.onerror = (e) => console.log("ERROR!", e);
-       // This is where we get the messages. The event is an object and we're interested in its `data` property
-        es.onmessage = (e) => {
 
-            console.log(">>>", e);
-            UpdateData(e.data);
-            toast(e.data);
-        };
-       // Whenever we're done with the data stream we must close the connection
-        return () => es.close();
-       }, []);
+    
+
+const [data, setData] = useState('data');
+useEffect(() => {
+    const evtSource = new EventSource('https://localhost:7183/sse');
+    evtSource.onmessage = (event) => {
+      if (event.data) {
+        setData(event.data);
+        toast(event.data);
+      }
+    };
+}, []);
+
+
     const notify = () => toast("Wow so easy!");
     return (
         <div>
